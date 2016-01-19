2cc3a694a60bcdb598af34f3c3e4932b097192f3
Erik Butcher <with_a_k@mac.com>
2016-01-19 11:47:13 -0700

    Create appointments, add procfile for future deployment

--- index.js
+++ index.js
@@ -2,46 +2,66 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const moment = require('moment');
 
 const path = require('path');
+const bodyParser = require('body-parser');
 const jsSHA = require('jsSHA');
+const pry = require('pryjs');
 var hasher = new jsSHA('SHA-512', 'TEXT');
 
+app.use( bodyParser.urlencoded({ extended: true }) );
 app.use(express.static('public'));
 app.set('view engine', 'jade');
 
 app.locals.title = "Bookmeet";
 
-var allSchedules = {};
+app.locals.allSchedules = {};
 
 app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.post('/schedules', function (req, res) {
+	if (!req.body.name) { return response.sendStatus(400); }
+
+  hasher.update(req.body.name + String(Math.random()) + '-admin');
+	var manageId = hasher.getHash('HEX').slice(0,8);
+	hasher.update(req.body.name + String(Math.random()) + '-bookers');
+	var bookerId = hasher.getHash('HEX').slice(0,10);
+
+  app.locals.allSchedules[manageId] = {
+		name: req.body.name,
+		manageId: manageId,
+		bookerId: bookerId,
+		appointments: []
+	}
+	console.log('Redirecting to ' + manageId);
+  res.redirect('/manage/' + manageId);
+});
+
 app.get('/manage/:id', function (req, res) {
-	res.sendFile(path.join(__dirname, '/public/manage.html'));
+	res.render('manage', {schedule: app.locals.allSchedules[req.params.id]});
 });
 
 app.get('/book/:id', function (req, res) {
-	res.sendFile(path.join(__dirname, '/public/book.html'));
+	res.render('book', {schedule: app.locals.allSchedules[req.params.id.slice(0,8)]});
 });
 
 io.on('connection', function (socket) {
   socket.on('message', function (channel, message) {
-  	console.log('Received ' + message['name']);
-  	if(channel === 'new schedule') {
-  		console.log('Creating new schedule...')
-  		hasher.update(message['name'] + '-admin');
-			var manageId = hasher.getHash('HEX').slice(0,8);
-			hasher.update(message['name'] + '-bookers');
-			var bookerId = hasher.getHash('HEX').slice(0,10);
-			allSchedules[manageId] = {
-				name: message['name'],
-				manageId: manageId,
-				bookerId: bookerId,
-				appointments: []
-			}
-			io.sockets.emit('returning', allSchedules[manageId]);
+  	console.log('Received message on ' + channel);
+  	if(channel.includes('new-slot', 9)) {
+  		var schedule = channel.slice(0,8);
+  		var datetime = moment.utc(message['datetime']);
+  		console.log('Creating new slot for ' + channel.slice(0,8) + ' on ' + message['datetime']);
+  		var appointment = {
+  			taken: false,
+  			date: datetime
+  		};
+  		app.locals.allSchedules[schedule]['appointments'].push(appointment);
+  		console.log(app.locals.allSchedules[schedule]['appointments']);
+  		io.sockets.emit(schedule, appointment);
   	}
   });
 });
--- public/application.js
+++ public/application.js
@@ -1,4 +1,5 @@
 const socket = io();
+// const relevantChannel = ;
 
 socket.on('connect', function () {
   console.log('Connection established.');
@@ -8,12 +9,10 @@ socket.on('returning', function (message) {
 	console.log(message);
 });
 
-$('.new-schedule').on('click', function(e) {
-	e.preventDefault();
-
-	var name = $('.new-schedule-name').val();
-	console.log('Sending ' + name);
-	socket.send('new schedule', {
-		name: name
+$('#make-new-slot').on('click', function(e) {
+	var datetime = $('#new-appointment-date').val() + 'T' + $('#new-appointment-time').val();
+	console.log('Sending ' + datetime);
+	socket.send($('#make-new-slot').attr('data-for') + '-new-slot', {
+		datetime: datetime
 	});
 });
